8448142d9cde11c11a75515456dfcf5aef49cf37
phariha <phariha@uw.edu>
2023-10-24 15:54:08 -0700

    app and all edits for homework

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BigGoButton
-Info 448 Homework
+
+# Details
+
+1. The entire screen should consist of a single pushbutton, labeled "Push Me!"
+2. The button should also have a green background, red text, the button text should be 40dp in size and italicized
+3. When I push the "Push Me!" button, the text should change to read "You have pushed me 1 time!"
+4. Each time I push the "Push Me!" button, the counter in the text should increment by 1
+5. ... and the text should read "You have pushed me 2 times!" "... 3 times!" and so on
---
+++ app/src/main/java/edu/uw/ischool/phariha/biggobutton/MainActivity.kt
@@ -0,0 +1,27 @@
+package edu.uw.ischool.phariha.biggobutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btnGo : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var count = 0
+
+        setContentView(R.layout.activity_main)
+
+        btnGo = this.findViewById<Button>(R.id.btnGo)
+        btnGo.setOnClickListener { it ->
+            count++
+            if (count == 1)
+                btnGo.text = "You have pushed me $count time!"
+            else
+                btnGo.text = "You have pushed me $count times!"
+        }
+
+    }
+}
